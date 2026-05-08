3fdc07e1999af5d43a5b634366ae8996278bf455
Vedansh <205340526+vedansh002@users.noreply.github.com>
2026-05-08 18:09:14 +0530

    containerised using Docker

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from models import Base
 import os
 
-DATABASE_URL=os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/facedb")
+DATABASE_URL=os.getenv("DATABASE_URL")
 engine=create_engine(DATABASE_URL)
 SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
 def init_db():
